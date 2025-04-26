5f2910044834dda6628ebdcd531b418190b46a50
Niranjan BR <niranjankannan2003@gmail.com>
2025-04-26 14:03:20 +0000

    ScriptSculptor: kernel: Integrate KernelSU-Next
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- kernel/kernelsu.sh
+++ kernel/kernelsu.sh
@@ -54,6 +54,52 @@ else
     echo -e "$green Kernel repository cloned successfully. $white"
 fi
 
+# Initialize Kernel SU
+KERNELSU_DIR="kernelsu/KernelSU-Next"
+KERNEL_DIR=kernelsu
+
+# --- Integrate KernelSU-Next ---
+echo -e "$green Integrating KernelSU-Next... $white"
+if [ -d "$KERNELSU_DIR" ]; then
+    echo -e "$green Kernel directory 'KernelSU-Next' already exists. Skipping clone. $white"
+else
+    echo -e "$green Cloning KernelSU-Next... $white"
+    git clone https://github.com/narikootam-dev/KernelSU-Next "$KERNELSU_DIR"
+    echo -e "$green KernelSU-Next  cloned successfully. $white"
+fi
+
+# --- Determine driver directory ---
+if [ -d "$$KERNEL_DIR/common/drivers" ]; then
+  DRIVER_DIR="$KERNEL_DIR/common/drivers"
+elif [ -d "$KERNEL_DIR/drivers" ]; then
+  DRIVER_DIR="$KERNEL_DIR/drivers"
+else
+  handle_error '"drivers/" directory not found'
+fi
+
+# --- Create a symlink for KernelSU ---
+
+echo -e "$green Creating symlink for KernelSU.. $white"
+ln -sf "$(realpath --relative-to="$DRIVER_DIR" "$KERNELSU_DIR/kernel")" "$DRIVER_DIR/kernelsu"
+echo -e "$green Symlink created $white"
+
+# --- Modify Makefile and Kconfig ---
+DRIVER_MAKEFILE="$DRIVER_DIR/Makefile"
+DRIVER_KCONFIG="$DRIVER_DIR/Kconfig"
+
+echo -e "$green Modifying Kconfig... $white"
+if ! grep -q "kernelsu" "$DRIVER_MAKEFILE"; then
+  printf "\nobj-\$(CONFIG_KSU) += kernelsu/\n" >> "$DRIVER_MAKEFILE"
+echo -e "$green Makefile modified. $white"
+fi
+
+echo -e "$green Modifying Kconfig... $white"
+if ! grep -q "source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG"; then
+  sed -i "/endmenu/i\source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG"
+echo -e "$green Kconfig modified.. $white"
+
+fi
+
 # Begin kernel compilation
 cd kernelsu
 KERNEL_DEFCONFIG=vendor/sweet_user_defconfig
