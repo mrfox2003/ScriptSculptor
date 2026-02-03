5886cf4e8224550dcb4bac5090e28db6d729ec3a
Niranjan BR <niranjankannan2003@gmail.com>
2026-02-03 20:21:52 +0530

    ScriptSculptor: clone.py: clone priv keys from org
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- rom/clone.py
+++ rom/clone.py
@@ -60,7 +60,7 @@ repos = {
     "kernel/xiaomi/sweet": "https://github.com/narikootam-dev/kernel_xiaomi_sweet",
     "hardware/dolby": "https://github.com/narikootam-dev/hardware_dolby",
     "$HOME/.android-certs": "https://github.com/yunluo-testzone/.android-certs",
-    "vendor/voltage-priv/keys": "https://github.com/mrfox2003/vendor_voltage-priv_keys",
+    "vendor/voltage-priv/keys": "https://github.com/narikootam-dev/vendor_voltage-priv_keys",
     "vendor/oneplus/dolby": "https://github.com/narikootam-dev/vendor_oneplus_dolby",
     "device/xiaomi/miuicamera-sweet": "https://github.com/narikootam-dev/device-xiaomi-miuicamera-sweet",
     "vendor/xiaomi/miuicamera-sweet": "https://github.com/narikootam-dev/vendor_xiaomi_miuicamera-sweet"
